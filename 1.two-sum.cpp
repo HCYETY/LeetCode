83157c3ab15af13e745ee80ce6dd645ce49228b1
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-05-09 22:37:42 +0800

    2021-05-09

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -1,6 +1,34 @@
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回它们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+
+示例 1：
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+
+示例 2：
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+
+示例 3：
+输入：nums = [3,3], target = 6
+输出：[0,1]
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
+        int i, j;
+        int len = nums.size();
+        for(i = 0; i < len-1; i++) {
+            for(j = i + 1; j < len; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {i,j};
     }
 };
