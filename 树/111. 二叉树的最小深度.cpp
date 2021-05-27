ae4491f81ab8dcbb8da3c0bcbc12b46b5d20c6c7
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-05-27 17:10:16 +0800

    求深度

---
+++ 树/111. 二叉树的最小深度.cpp
@@ -0,0 +1,55 @@
+/*
+给定一个二叉树，找出其最小深度。
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明：叶子节点是指没有子节点的节点。
+
+示例 1：
+输入：root = [3,9,20,null,null,15,7]
+输出：2
+
+示例 2：
+输入：root = [2,null,3,null,4,null,5,null,6]
+输出：5
+
+提示：
+树中节点数的范围在 [0, 105] 内
+-1000 <= Node.val <= 1000
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        // 首先，节点为空，直接返回0
+        if(root == nullptr) return 0;
+        int m1 = minDepth(root->left);
+        int m2 = minDepth(root->right);
+        //1.如果左孩子和右孩子都为空的情况，直接返回m1+m2+1
+        //2.如果左孩子和右孩子存在其中一个为空的情况，也直接返回m1+m2+1
+        //3.如果都不为空，返回较小深度+1
+        return root->left == nullptr || root->right == nullptr ? m1 + m2 + 1 : min(m1,m2) + 1;
+    }
+};
