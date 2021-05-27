53a9c5c4fef94ec632ce691d79d5eca4f77fd834
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-05-27 16:25:15 +0800

    二叉树后序遍历的迭代

--- 树/145. 二叉树的后序遍历.cpp
+++ 树/145. 二叉树的后序遍历.cpp
@@ -40,3 +40,23 @@ public:
     }
 };
 //迭代
+//先序遍历是中左右，后续遍历是左右中，那么我们只需要调整一下先序遍历的代码顺序，就变成中右左的遍历顺序，然后在反转result数组，输出的结果顺序就是左右中了，
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+    	  if (root == NULL) return result;
+        stack<TreeNode*> st;
+        vector<int> result;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            if (node->left) st.push(node->left); // 相对于前序遍历，这更改一下入栈顺序 （空节点不入栈）
+            if (node->right) st.push(node->right); // 空节点不入栈
+        }
+//      reverse函数用于反转在[first,last)范围内的顺序（包括first指向的元素，不包括last指向的元素），reverse函数没有返回值
+        reverse(result.begin(), result.end()); // 将结果反转之后就是左右中的顺序了
+        return result;
+    }
+};
