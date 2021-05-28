df6d9168df8c669e3ce9c11d03239b4ef6e833dc
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-05-28 21:44:36 +0800

    单调栈

--- 栈/496. 下一个更大元素 I.cpp
+++ 栈/496. 下一个更大元素 I.cpp
@@ -28,19 +28,35 @@ nums1 中的所有整数同样出现在 nums2 中
 */
 
 
+//思路：先找出 nums1 的数字在 nums2 中对应的下一个更大的数字找出来，然后放到哈希表中，以供后面 nums1 直接使用即可
+
+//具体流程如下：
+//1.创建一个临时栈，一个哈希表，然后遍历 nums2。
+//2.若当前栈无数据，则当前数字入栈备用。
+//3.若当前栈有数据，则用当前数字与栈顶比较：
+//  3.1 当前数字 > 栈顶，代表栈顶对应下一个更大的数字就是当前数字，则将该组数字对应关系，记录到哈希表。
+//  3.2 当前数字 < 栈顶，当前数字压入栈，供后续数字判断使用。
+//4.这样，我们就可以看到哈希表中存在部分 nums2 数字的对应关系了，而栈中留下的数字，代表无下一个更大的数字，我们全部赋值为 -1 ，然后存入哈希表即可。
+//5.遍历 nums1nums1，直接询问哈希表拿对应关系即可。
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans(nums1.size());
+        vector<int> ans;
+        unordered_map<int,int>num_map;
         stack<int> mystack;
-        for(int i = nums2.size(); i >= 0; i--) {
-            while(!mystack.empty() && nums2[i] >= mystack.top()) {
+        for(int i = 0; i < nums2.size(); i++) {
+            while(!mystack.empty() && mystack.top() < nums2[i]) {
+                num_map[mystack.top()] = nums2[i];
                 mystack.pop();
             }
             mystack.push(nums2[i]);
         }
+        while(!mystack.empty()) {
+            num_map[mystack.top()] = -1;
+            mystack.pop();
+        }
         for(int i = 0; i < nums1.size(); i++) {
-            ans[i] = mystack.empty() ? -1 : mystack.top();
+            ans.push_back(num_map[nums1[i]]);
         }
         return ans;
     }
