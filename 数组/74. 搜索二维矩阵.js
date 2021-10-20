8b738ca8f27015cb20ddded8ae5272b55e6705e3
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-10-20 22:29:31 +0800

    依靠特定条件将矩阵拼接成一行，再进行二分查找

---
+++ 数组/74. 搜索二维矩阵.js
@@ -0,0 +1,44 @@
+/*
+编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+每行中的整数从左到右按升序排列。
+每行的第一个整数大于前一行的最后一个整数。
+
+示例 1：
+输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+输出：true
+
+示例 2：
+输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+输出：false
+
+提示：
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+*/
+
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+// 将矩阵拼接成一行（虚拟的），在一行上二分查找。
+// 获取值的时候要将 index 映射到实际的 index（原矩阵的行和列）
+var searchMatrix = function(matrix, target) {
+  const m = matrix.length, n = matrix[0].length;
+  let low = 0, high = m * n - 1;
+  while (low <= high) {
+    const mid = Math.floor((high - low) / 2) + low;
+    const x = matrix[Math.floor(mid / n)][mid % n];
+    if (x < target) {
+      low = mid + 1;
+    } else if (x > target) {
+      high = mid - 1;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
